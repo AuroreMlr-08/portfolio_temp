a9f67f370f1956a109b9a9ba5989800ec8cf53f9
AuroreMlr-08 <aurore.mlr08@gmail.com>
2020-03-29 20:07:04 +0200

    [index] change link css

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>Portfolio | MARLIER Aurore</title>
-    <link href="styles/scss/style.scss" rel="stylesheet">
+    <link href="styles/css/style.css" rel="stylesheet">
 </head>
 <body><!--
 <div class="loader" >
